5971232b9909b03131084fcea5a8bb81a9b0ea04
Ashes Das <127014640+Ashes2004@users.noreply.github.com>
2024-09-24 08:26:55 +0530

    .

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ const corsOptions = {
     }
   },
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST','PATCH', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
 
